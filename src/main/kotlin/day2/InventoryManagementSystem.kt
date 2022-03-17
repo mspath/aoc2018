018ea72d5e78332118ee8809ed1ae76c3405f6a2
Markus Spath <markus@hackr.de>
2022-03-17 09:27:05 +0100

    day 2 pt.2

--- src/main/kotlin/day2/InventoryManagementSystem.kt
+++ src/main/kotlin/day2/InventoryManagementSystem.kt
@@ -7,6 +7,8 @@ fun main() {
     val input = File("data/day2/input.txt").readLines()
     val resultBreakfast = breakfast(input)
     println(resultBreakfast) // 6000
+    val resultLunch = lunch(input)
+    println(resultLunch)
 }
 
 fun containsTwos(s: String): Boolean {
@@ -23,4 +25,27 @@ fun breakfast(input: List<String>): Int {
     val twos = input.count { containsTwos(it) }
     val threes = input.count { containsThrees(it) }
     return twos * threes
+}
+
+fun countDifferences(s1: String, s2: String): Int {
+    assert(s1.length == s2.length)
+    return s1.zip(s2).count { it.first != it.second }
+}
+
+fun collectCommonChars(s1: String, s2: String): String {
+    assert(s1.length == s2.length)
+    val s = s1.zip(s2).filter { it.first == it.second }.map { it.first }
+    return s.joinToString("")
+}
+
+fun lunch(input: List<String>): String {
+    for (i in 0 until input.size) {
+        for (j in i + 1 until input.size) {
+            val diff = countDifferences(input[i], input[j])
+            if (diff == 1) {
+                return collectCommonChars(input[i], input[j])
+            }
+        }
+    }
+    return "no match found"
 }
