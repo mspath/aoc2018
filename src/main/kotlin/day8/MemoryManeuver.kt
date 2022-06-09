e38aa5827123b1453637f036cebb829e12f5b202
Markus Spath <markus@hackr.de>
2022-06-09 09:43:53 +0200

    day 8 pt2

--- src/main/kotlin/day8/MemoryManeuver.kt
+++ src/main/kotlin/day8/MemoryManeuver.kt
@@ -11,6 +11,13 @@ data class Tree(val children: List<Tree>, val meta: List<Int>) {
 
     fun sumMeta(): Int = meta.sum() + children.sumOf { it.sumMeta() }
 
+    fun value(): Int {
+        if (children.isEmpty()) return meta.sum()
+        else {
+            return meta.sumOf { children.getOrNull(it - 1)?.value() ?: 0 }
+        }
+    }
+
     companion object {
         fun fromIterator(input: Iterator<Int>): Tree {
             val kids: Int = input.next()
@@ -27,4 +34,6 @@ fun breakfast(input: List<Int>) {
     println(tree)
     val meta = tree.sumMeta()
     println(meta)
+    val value = tree.value()
+    println(value)
 }
